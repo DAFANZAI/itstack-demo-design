2f71e1803f29f2cecabdde73c626da9f7d77bd52
feifan <347888927@qq.com>
2024-09-08 00:37:51 +0800

    工厂模式学习笔记

--- itstack-demo-design-1-02/src/main/java/org/itstack/demo/design/StoreFactory.java
+++ itstack-demo-design-1-02/src/main/java/org/itstack/demo/design/StoreFactory.java
@@ -7,6 +7,12 @@ import org.itstack.demo.design.store.impl.GoodsCommodityService;
 
 public class StoreFactory {
 
+    /**
+     * 把不同类别的业务方法抽象出来到不同业务类，通过工厂方式创建业务类，避免创建者和具体业务逻辑耦合。
+     * 单一职责、开闭原则
+     * @param commodityType
+     * @return
+     */
     public ICommodity getCommodityService(Integer commodityType) {
         if (null == commodityType) return null;
         if (1 == commodityType) return new CouponCommodityService();
